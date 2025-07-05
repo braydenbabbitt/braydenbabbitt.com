490d26bd24944c20ea37a44e8bca117fee98b8f2
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-07-04 21:27:50 -0600

    feat: updated logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE_BASE = "Brayden Babbitt";
+export const DEFAULT_SITE_DESCRIPTION =
+  "Brayden Babbitt's personal website, showcasing his projects, blog, and other content.";
