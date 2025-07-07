25a8c5b5a5ca52a1b667b4c295a2a9fe9bf04ccd
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-07-06 19:57:02 -0600

    feat: init full site

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE_BASE = "Brayden Babbitt";
+export const DEFAULT_SITE_DESCRIPTION =
+  "Brayden Babbitt's personal website, showcasing his projects, blog, and other content.";
