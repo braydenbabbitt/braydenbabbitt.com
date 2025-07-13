8aefb298d877570843ae938f44f29c3fd1181105
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-07-13 11:38:32 -0600

    feat: added projects page with Modular Music (#2)

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,24 @@
-export const SITE_TITLE_BASE = "Brayden Babbitt";
+export const SITE_TITLE_BASE = "Brayden Babbitt" as const;
 export const DEFAULT_SITE_DESCRIPTION =
-  "Brayden Babbitt's personal website, showcasing his projects, blog, and other content.";
+  "Brayden Babbitt's personal website, showcasing his projects, blog, and other content." as const;
+
+export const TAGS = {
+  React: {
+    color: "#61DAFB",
+  },
+  Typescript: {
+    color: "#3178C6",
+  },
+  Postgres: {
+    color: "#336791",
+  },
+  Deno: {
+    color: "#202020",
+  },
+  Supabase: {
+    color: "#3ECF8E",
+  },
+  Astro: {
+    color: "#d83333",
+  },
+} as const satisfies Record<string, { color: string }>;
